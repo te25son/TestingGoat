8d7986411833ceb398214f95df44befb1608b4c3
Timothy <te25son@gmail.com>
2019-04-03 21:37:30 +0200

    FT now checks that we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -23,16 +25,30 @@ class NewVisitorTest(unittest.TestCase):
         # She notices that the header and page title mention something about
         # to-do lists, so she must be in the right spot.
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item right from the get-go.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item'
+        )
 
         # She types in "Buy himalayan salt". (Sandra heard about the amazing benifits of
         # himalayan salt from the same friend who told her about the app. She's sceptical
         # but scientific. She decides to try for herself.)
+        inputbox.send_keys('Buy himalayan salt')
 
         # When she hits enter, the page updates. The page now lists "1: Buy himalayan salt"
         # as an item on her to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy himalayan salt' for row in rows)
+        )
 
         # There is still a text box inviting her to add another to-do item. She types
         # "Download the audiobooks - 'The Amazing Benefits of Himalayan Salt' and 'Himalayan
@@ -47,6 +63,7 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL and... her to-do list is still there!
 
         # Satisfied, she begins to make herself a bath.
+        self.fail("Finish the test!")
 
 
 if __name__ == '__main__':
